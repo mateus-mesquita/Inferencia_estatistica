5c6fac074b688a59579f4922669b257eb69ef0fd
mateus-mesquita <mateussantoz522@gmail.com>
2025-05-11 15:00:57 -0300

    IC usando distribuição normal padrão usando R

---
+++ intervalos_de_confianca/IC_norm.R
@@ -0,0 +1,27 @@
+# Criando função de intervalo de confiança para distribuição normal
+
+erro.medio = function(dp,n) dp/sqrt(n)
+
+IC.Norm <- function(dados, conf = 0.95){
+  # alfa/2
+  alfa = qnorm(1-conf/2)
+  erro.amostral = alfa*erro.medio(sd(dados),length(dados))
+  
+  ic = c(
+    "Limite inferior" = mean(dados) - erro.amostral,
+    "Limite Superior" = mean(dados) + erro.amostral
+  )
+  return(ic)
+}
+
+# Para casos em que não possui amostra
+IC.Norm2 <- function(media,dp,n,conf = 0.95){
+  alfa = qnorm(1-conf/2)
+  erro.amostral = alfa*erro.medio(dp,n)
+  
+  ic = c(
+    "Limite inferior" = mean(dados) - erro.amostral,
+    "Limite Superior" = mean(dados) + erro.amostral
+  )
+  return(ic)
+}
