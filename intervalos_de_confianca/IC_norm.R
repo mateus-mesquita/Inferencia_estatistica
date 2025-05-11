33410fa4e0ff4f5c115a7115664b453fc7e7afde
mateus-mesquita <mateussantoz522@gmail.com>
2025-05-11 18:57:03 -0300

    correção

--- intervalos_de_confianca/IC_norm.R
+++ intervalos_de_confianca/IC_norm.R
@@ -4,7 +4,7 @@ erro.medio = function(dp,n) dp/sqrt(n)
 
 IC.Norm <- function(dados, conf = 0.95){
   # alfa/2
-  alfa = qnorm(1-conf/2)
+  alfa = qnorm(1-(1-conf)/2)
   erro.amostral = alfa*erro.medio(sd(dados),length(dados))
   
   ic = c(
@@ -16,12 +16,13 @@ IC.Norm <- function(dados, conf = 0.95){
 
 # Para casos em que não possui amostra
 IC.Norm2 <- function(media,dp,n,conf = 0.95){
-  alfa = qnorm(1-conf/2)
+  alfa = qnorm(1-(1-conf)/2)
   erro.amostral = alfa*erro.medio(dp,n)
   
   ic = c(
-    "Limite inferior" = mean(dados) - erro.amostral,
-    "Limite Superior" = mean(dados) + erro.amostral
+    "Limite inferior" = media - erro.amostral,
+    "Limite Superior" = media + erro.amostral
   )
   return(ic)
 }
+
